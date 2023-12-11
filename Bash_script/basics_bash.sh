10c98e9193ddb93eb973a9c09140e36d0c7f5737
John Njuguna <johnmnjuguna89@gmail.com>
2023-12-11 12:07:20 +0300

    basics of bash scripting

---
+++ Bash_script/basics_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello World!"
+echo "My current working directory is: "
+pwd
+ls
+
+# Basics of math in bash
+expr 30 + 10
+expr 30 - 10
+expr 30 / 10
+expr 30 % 10
+expr 30 \* 10
